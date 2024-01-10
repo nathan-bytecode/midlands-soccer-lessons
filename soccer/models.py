647acd32f06574177425fec6ae72d3849c8c4079
Nathan Nicholson <nathan.bytecode@gmail.com>
2024-01-10 10:14:00 +0000

    create account model and add pillow package

--- soccer/models.py
+++ soccer/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Account(models.Model):
+    name = models.CharField(max_length=200, unique=True)
+    email = models.EmailField(max_length=200, unique=True)
+    phone = models.IntegerField(unique=True)
+    photo = models.ImageField(unique=True)

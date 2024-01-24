fa3186df227f5e04adcd7b9946d838202d86853f
Nathan Nicholson <nathan.bytecode@gmail.com>
2024-01-24 12:31:35 +0000

    add course models

--- soccer/models.py
+++ soccer/models.py
@@ -10,5 +10,31 @@ class Account(models.Model):
     photo = models.ImageField(unique=True)
     author = models.ForeignKey(
     User, on_delete=models.CASCADE, related_name="account"
-
 )
+
+# Course 1 model for boys aged 7+
+class Course1(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.TextField(max_length=200)
+    price = models.IntegerField()
+
+    def __str__(self):
+        return self.title
+
+# Course 2 model for boys aged 14+
+class Course2(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.TextField(max_length=200)
+    price = models.IntegerField()
+
+    def __str__(self):
+        return self.title
+
+# Course 3 model for girls aged 9+
+class Course3(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.TextField(max_length=200)
+    price = models.IntegerField()
+
+    def __str__(self):
+        return self.title

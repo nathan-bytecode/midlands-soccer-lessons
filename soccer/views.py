d3260fc870c9265f3e69c4bb3ee3fb62c2af0712
Nathan Nicholson <nathan.bytecode@gmail.com>
2024-02-23 12:26:57 +0000

    ran views and models files through python checker

--- soccer/views.py
+++ soccer/views.py
@@ -8,11 +8,14 @@ from django.contrib import messages
 
 # Create your views here.
 
+
 def index(request):
     return render(request, 'soccer/index.html')
 
+
 def account(request):
     return render(request, 'soccer/templates/account/signup.html')
+
 
 def profile(request):
     """
@@ -27,51 +30,55 @@ def profile(request):
         form.save()
     elif 'delete' in request.POST:
         pk = request.POST.get('delete')
-        booking = Booking.objects.get(id=pk)
+        booking = Booking.objects.get(id = pk)
         booking.delete()
     elif 'edit' in request.POST:
         pk = request.POST.get('edit')
-        booking = Booking.objects.get(id=pk)
-        form = BookingForm(instance=booking) 
+        booking = Booking.objects.get(id = pk)
+        form = BookingForm(instance = booking) 
     context['form'] = form
     return render(request, "soccer/profile.html", {'bookings': bookings})
 
 
 def edit_booking(request, booking_id):
-    booking = get_object_or_404(Booking, id=booking_id)
-    form = BookingForm(instance=booking)
+    booking = get_object_or_404(Booking, id = booking_id)
+    form = BookingForm(instance = booking)
     if request.POST:
-        form = BookingForm(request.POST, instance=booking)
+        form = BookingForm(request.POST, instance = booking)
         if form.is_valid():
-            form.save() # save method called upon to update booking info #
-            messages.success(request, # upon update, message displays to nofify user #
-                             'Your booking request has been updated.')
+            # save method called upon to update booking info #
+            form.save()
+            # upon update, message displays to nofify user #
+            messages.success(request,
+                'Your booking request has been updated.')
         return redirect('profile')
-    booking = BookingForm(instance=booking)
+    booking = BookingForm(instance = booking)
     context = {
-        'form': form
-    }
+        'form': form }
     return render(request, 'soccer/edit_booking.html', context)
 
+
 def delete_booking(request, booking_id):
-    booking = get_object_or_404(Booking, id=booking_id)
+    booking = get_object_or_404(Booking, id = booking_id)
     booking.delete() # delete method called upon to cancel user's booking #
     messages.warning(request,
-                     'Your booking has been cancelled.')
+        'Your booking has been cancelled.')
     return redirect('profile')
+
 
 def courses(request):
     return render(request, 'soccer/courses.html')
 
+
 def bookings(request):
     context = {}
     form = BookingForm()
-    if request.method =='POST':
+    if request.method == 'POST':
         if 'submit' in request.POST:
             form = BookingForm(request.POST)
             if form.is_valid():
                 form.instance.user = (
-                User.objects.get(username=request.user))
+                User.objects.get(username = request.user))
                 form.save()
                 print("form saved")
             elif 'delete' in request.POST:
@@ -79,6 +86,5 @@ def bookings(request):
                 print('other')
         else:
             print("form not saved or started", request.POST)
-    
-        context['form'] = form
+            context['form'] = form
     return render(request, 'soccer/bookings.html', {'form': BookingForm})
